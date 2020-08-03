41a690802433cd84ed4b3532cc8a39de0e9ba6a9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-03 14:49:30 +0300

    Initial Commit

---
+++ GuessTheColor/GuessTheColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheColorApp.swift
+//  GuessTheColor
+//
+//  Created by ramil on 03.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
